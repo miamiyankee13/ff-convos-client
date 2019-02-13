33e88d7da7004394db1127020befda6a8a0046b5
Anthony Damico <anthony.damico721@gmail.com>
2019-02-13 14:12:19 -0500

    added PlayersOptions

---
+++ src/components/player-options.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+export class PlayerOptions extends React.Component {
+    render() {
+        //Create JSX options for players
+        const playerOptions = this.props.players.map((player, index) => {
+            return <option key={`player-${index}`} value={index}>{player.name}</option>
+        });
+
+        return playerOptions
+    }
+}
+
+const mapStateToProps = state => ({
+    players: state.playerData.players
+});
+
+export default connect(mapStateToProps)(PlayerOptions);
