01465c33c228de2239b17c3d8c6ac72b4b55f87a
Anthony Damico <anthony.damico721@gmail.com>
2019-02-15 07:11:35 -0500

    edited mounting dispatch tests

--- src/components/tests/players-page.test.js
+++ src/components/tests/players-page.test.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { PlayersPage } from '../players-page';
 
-// Mock the async fetchPlayers action
+// Mock the async fetchPlayers & fetchUserPlayers actions
 const mockFetchPlayersAction = {
     type: 'FETCH_PLAYERS'
 };
+
+const mockFetchUserPlayersAction = {
+    type: 'FETCH_USER_PLAYERS'
+}
+
 jest.mock('../../actions/players', () => Object.assign({},
     require.requireActual('../../actions/players'),
     {
         fetchPlayers: jest.fn().mockImplementation(() => {
             return mockFetchPlayersAction;
+        }),
+        fetchUserPlayers: jest.fn().mockImplementation(() => {
+            return mockFetchUserPlayersAction;
         })
     }
 ));
@@ -25,7 +33,7 @@ describe('<PlayersPage />', function() {
         shallow(<PlayersPage userPlayers={userPlayers} dispatch={dispatch} />);
     });
 
-    it('Dispatches fetchPlayers on mount', function() {
+    it('Dispatches fetchPlayers & fetchUserPlayers on mount', function() {
         const dispatch = jest.fn()
         const userPlayers = [
             { name: 'Dan Marino' },
@@ -33,5 +41,6 @@ describe('<PlayersPage />', function() {
         ];
         shallow(<PlayersPage userPlayers={userPlayers} dispatch={dispatch} />);
         expect(dispatch).toHaveBeenCalledWith(mockFetchPlayersAction);
+        expect(dispatch).toHaveBeenCalledWith(mockFetchUserPlayersAction);
     });
 });
